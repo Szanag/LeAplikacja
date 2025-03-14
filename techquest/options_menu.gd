a414919514c3e26c589d4cb65219264e11077db0
CloneKosina2 <kubakosina30@gmail.com>
2025-03-14 13:43:49 +0100

    nwm

--- techquest/options_menu.gd
+++ techquest/options_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_volume_value_changed(value: float) -> void:
+	pass # Replace with function body.
